3e839ba107a4d970c037eda667dd88d835321b95
pwatabe <paulwatabe@gmail.com>
2015-09-09 16:23:29 -0700

    Warcraft III assignment

---
+++ lib/barracks.rb
@@ -0,0 +1,33 @@
+# require 'pry'
+class Barracks
+  attr_accessor :gold, :food
+  def initialize
+    @gold = 1000
+    @food = 80
+  end
+
+  def train_footman
+    if can_train_footman?
+      @gold -= 135
+      @food -= 2
+      Footman.new
+    end
+  end
+
+  def can_train_footman?
+    food >= 2 && gold >= 135
+  end
+
+  def train_peasant
+    if can_train_peasant?
+      @gold -= 90
+      @food -= 5
+      Peasant.new
+    end    
+  end
+
+  def can_train_peasant?
+    food >= 5 && gold >= 90
+  end
+
+end
---
+++ lib/unit.rb
@@ -0,0 +1,15 @@
+class Unit
+  # attr_accessor :health_points, :attack_power
+  attr_reader :health_points
+  def initialize(health_points,attack_power)
+    @health_points = health_points
+    @attack_power = attack_power
+  end
+  def damage(num)
+    @health_points -= num
+  end
+  def attack!(enemy)
+    enemy.damage(@attack_power)
+  end
+end
+unit = Unit.new(30,5)
